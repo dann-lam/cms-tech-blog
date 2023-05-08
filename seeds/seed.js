a794a79f2af3f32e7d25975f88584367821b8235
Daniel Lam <Dann.lam@gmail.com>
2023-05-07 17:06:37 -0700

    first init

---
+++ seeds/seed.js
@@ -0,0 +1,24 @@
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
+
+const userData = require("./userData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+  });
+
+  await Post.bulkCreate(postData, {
+    individualHooks: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
